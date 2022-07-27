7a5e6d8061b12c19066b2c09fbfad7344e04e541
Ethan Armstrong <w.ethan.armstrong@gmail.com>
2022-07-26 23:03:41 -0700

    added api endpoints for continuity

--- src/api.rs
+++ src/api.rs
@@ -9,21 +9,47 @@ use std::sync::{Arc, Mutex};
 
 pub struct Data {
     pub altitude: Vec<(f32, f32)>,
+    pub cont_main: Vec<(f32, f32)>,
+    pub cont_droug: Vec<(f32, f32)>,
 }
 
 impl Data {
     pub fn new() -> Data {
-        Data {altitude: vec![(0.0, 0.0), (1.0, 1.0), (2.0, 2.0)]}
+        Data {altitude: vec![], cont_main: vec![], cont_droug: vec![]}
     }
 }
 
 pub type TData = Arc<Mutex<Data>>;
 
+fn compile_vec(data: &mut Vec<(f32, f32)>, points: usize, is_neg: bool) -> Vec<(f32, f32)> {
+    if points > data.len() && !is_neg {
+        return vec![];
+    }
+
+    let res: Vec<(f32, f32)>;
+    if is_neg {
+        let index: usize;
+        if points > data.len() {
+            index = 0;
+        }
+        else {
+            index = data.len() - points;
+        }
+
+        res = data[index..].to_vec();
+       
+    }
+    else {
+        res = data[points as usize..].to_vec();
+    }
+
+    return res;
+}
 
 #[rocket::get("/api/<field>/<points>")]
 fn handle_api(state: &State<TData>, field: &str, points: i32) -> Json<Vec<(f32, f32)>> {
     let data = Arc::clone(&state);
-    let data = data.lock().expect("could not lock mutex");
+    let mut data = data.lock().expect("could not lock mutex");
     
     let is_neg: bool = points < 0;
     let points: i32 = if is_neg {points*-1} else {points};
@@ -31,34 +57,24 @@ fn handle_api(state: &State<TData>, field: &str, points: i32) -> Json<Vec<(f32, 
 
     println!("{}, -: {}", points, is_neg);
 
-    if field == "alt" {
-        if points > data.altitude.len() && !is_neg {
-            return Json(vec![]);
-        }
+    return match field {
+        "alt" => {
+            Json(compile_vec(&mut data.altitude, points, is_neg))
+        },
 
-        let res: Vec<(f32, f32)>;
-        if is_neg {
-            let index: usize;
-            if points > data.altitude.len() {
-                index = 0;
-            }
-            else {
-                index = data.altitude.len() - points;
-            }
+        "cont_main" => {
+            Json(compile_vec(&mut data.cont_main, points, is_neg))
+        },
 
-            res = data.altitude[index..].to_vec();
-           
-        }
-        else {
-            res = data.altitude[points as usize..].to_vec();
-        }
+        "cont_droug" => {
+            Json(compile_vec(&mut data.cont_droug, points, is_neg))
+        },
 
-        return Json(res);
-
+        _ => {
+            Json(vec![])
+        },
+        
     }
-
-
-    Json(vec![])
 }
 
 #[rocket::get("/cmd/<cmd>")]
@@ -81,22 +97,6 @@ fn shutdown(shutdown: Shutdown) -> &'static str {
 
 
 pub fn start_api(data: TData) {
-    //let data = Data {altitude: vec![(0.0,0.0), (1.0, 1.0), (2.0, 2.0), (3.0, 3.0)]};
-    //let thread_data: TData = Arc::new(Mutex::new(data));
-
-
-    /*let res = rocket::build()
-        .mount("/", rocket::routes![handle_api, handle_cmd, shutdown])
-        .manage(Arc::clone(&DATA))
-        .launch()
-        .await;
-
-    
-    match res {
-        Ok(_) => {},
-        Err(_) => {panic!("could not start api")},
-    };*/
-
     rocket::tokio::runtime::Builder::new_multi_thread()
         .worker_threads(Config::from(Config::figment()).workers)
         // NOTE: graceful shutdown depends on the "rocket-worker" prefix.
